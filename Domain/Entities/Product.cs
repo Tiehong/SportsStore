f514e19b59e725997eaba0950d7279d20de6986d
Tiehong Xiao <tiehong@chiwater.com>
2017-11-20 20:03:34 -0500

    Init Repo

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int ProductID { get; set; }
+        [Required(ErrorMessage = "Please enter a produce name")]
+        public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a produce description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Please enter a produce category")]
+        public string Category { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
+    }
+}
